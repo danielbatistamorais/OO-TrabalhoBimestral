3a82b0e1b49b2aedd85f36bfcc0f5976f5638dc7
Bruna <brunloyola02@gmail.com>
2019-06-11 13:46:19 -0300

    COMMIT TELA INICIAL

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("/sample/view/telaInicial.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/telaInicial.fxml"));
         primaryStage.setTitle("QUIZ");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
